7e065cc67e6d347644c2aeb03e400f57ccccb070
Alexander Zabelin <zabelinn@yandex.ru>
2017-12-20 16:37:08 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -2,7 +2,10 @@ package ru.job4j;
 
 
 public class Calculate {
-	
+	/**
+	* Метод - точка входа в программу
+	*@param args Массив параметров вызова программы
+	*/
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world.");		
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Alexander Zabelin (zabelinn@yandex.ru)
+* @version 0.1
+* @since 0.1
+*/
+
+package ru.job4j;
