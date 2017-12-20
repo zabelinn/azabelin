35aa725be4ca7885a5f988c4334d164c5429d527
Alexander Zabelin <zabelinn@yandex.ru>
2017-12-20 18:41:34 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -10,8 +10,7 @@ public class Calculate {
 	* Метод - точка входа в программу
 	*@param args Массив параметров вызова программы
 	*/
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Hello world.");		
 	}	
 	
