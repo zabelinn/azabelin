676a8e2215c44be34739c6b23970e6348814c1da
Alexander Zabelin <zabelinn@yandex.ru>
2017-12-21 23:19:58 +0300

    исправил ошибки найденный mvn validate

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -4,23 +4,23 @@ public class Calculator {
 
     private double result;
 
-    void add(double first, double second){
+    void add(double first, double second) {
         this.result = first + second;
     }
 
-    void substract(double first, double second){
+    void substract(double first, double second) {
         this.result = first - second;
     }
 
-    void div(double first, double second){
+    void div(double first, double second) {
         this.result = first / second;
     }
 
-    void multiple(double first, double second){
+    void multiple(double first, double second) {
         this.result = first * second;
     }
 
-    public double getResult(){
+    public double getResult() {
         return this.result;
     }
 }
