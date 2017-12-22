4a8640ae4d4b8f794cb6aebc62ae818bc28e455f
Alexander Zabelin <zabelinn@yandex.ru>
2017-12-22 14:55:58 +0300

    3.1. Глупый бот. [#31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+/**
+ *Class DummyBot
+ *@author Zabelin Alexander
+ *@since 0.1
+ */
+public class DummyBot {
+
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl;
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        } else {
+            rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        }
+
+        return rsl;
+    }
+}
